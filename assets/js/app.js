c4faacba35de85fa5e5e6025c34b72a159a7677b
henry <79429875+Henrycaba471@users.noreply.github.com>
2021-07-24 21:14:30 -0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,25 +11,52 @@ const d = document,
 
 d.addEventListener("click", (e) => {
     if (e.target.matches("#send")) {
-        let random = Math.round(Math.random() * 2);
+        let random = Math.round(Math.random() * 8);
         e.preventDefault();
         if (EqLocal.value === "") {
-            return result.innerHTML = ("No has ingresado el equipo Local")
+            return result.innerHTML = ("No has ingresado el equipo Local 🤷‍♂️")
         }
         if (EqVisitante.value === "") {
-            return result.innerHTML = ("No has ingresado el equipo Visitante")
+            return result.innerHTML = ("No has ingresado el equipo Visitante 🤷‍♂️")
         }
-
+        console.log(random);
         result.innerHTML = (`Local: <h3 class="local1">${EqLocal.value}</h3> <img src="assets/images/icons/vs.png" alt="vs" class="vs"/> <br> Visitante: <h3 class="visitante1">${EqVisitante.value}</h3>`);
 
-        if (random === 0) {
-            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: ${EqLocal.value}</h3>`);
+        if (random <= 2) {
+            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: ${EqLocal.value}</h3>`),
+            EqLocal.value = "",
+            EqVisitante.value = "";
         }
-        if (random === 1) {
-            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: Empate</h3>`);
+        if (random > 2 && random <= 5) {
+            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: Empate</h3>`),
+            EqLocal.value = "",
+            EqVisitante.value = "";
         }
-        if (random === 2) {
-            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: ${EqVisitante.value}</h3>`);
+        if (random > 5 && random <= 8) {
+            return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: ${EqVisitante.value}</h3>`),
+            EqLocal.value = "",
+            EqVisitante.value = "";
         }
     }
 });
+
+function menuHamburguer(panelBtn,panel,linkMenu){
+
+    const d = document;
+
+    d.addEventListener('click', (e) => {
+
+        if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+            d.querySelector(panel).classList.toggle('is-active');
+            d.querySelector(panelBtn).classList.toggle('is-active');
+        }
+
+        if (e.target.matches(linkMenu)) {
+            d.querySelector(panel).classList.remove('is-active');
+            d.querySelector(panelBtn).classList.remove('is-active');
+        }
+
+    });
+}
+menuHamburguer('.panel-btn', '.panel', '.nav a');
+
