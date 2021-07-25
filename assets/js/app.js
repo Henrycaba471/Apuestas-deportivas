6dbe244978aa23eec20a4b4fb0c642e5877460a6
henry <79429875+Henrycaba471@users.noreply.github.com>
2021-07-25 06:22:18 -0500

    Update app.js
    Actualizndo pequeños detalles

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,20 +14,20 @@ d.addEventListener("click", (e) => {
         let random = Math.round(Math.random() * 8);
         e.preventDefault();
         if (EqLocal.value === "") {
-            return result.innerHTML = ("No has ingresado el equipo Local 🤷‍♂️")
+            return result.innerText = ("No has ingresado el equipo Local 🤷‍♂️")
         }
         if (EqVisitante.value === "") {
-            return result.innerHTML = ("No has ingresado el equipo Visitante 🤷‍♂️")
+            return result.innerText= ("No has ingresado el equipo Visitante 🤷‍♂️")
         }
         console.log(random);
         result.innerHTML = (`Local: <h3 class="local1">${EqLocal.value}</h3> <img src="assets/images/icons/vs.png" alt="vs" class="vs"/> <br> Visitante: <h3 class="visitante1">${EqVisitante.value}</h3>`);
 
-        if (random <= 2) {
+        if (random <= 3) {
             return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: ${EqLocal.value}</h3>`),
             EqLocal.value = "",
             EqVisitante.value = "";
         }
-        if (random > 2 && random <= 5) {
+        if (random > 3 && random <= 5) {
             return apuesta.innerHTML = (`<h3 class="apuesta1">Apuesta: Empate</h3>`),
             EqLocal.value = "",
             EqVisitante.value = "";
@@ -41,21 +41,15 @@ d.addEventListener("click", (e) => {
 });
 
 function menuHamburguer(panelBtn,panel,linkMenu){
-
-    const d = document;
-
     d.addEventListener('click', (e) => {
-
         if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
             d.querySelector(panel).classList.toggle('is-active');
             d.querySelector(panelBtn).classList.toggle('is-active');
         }
-
         if (e.target.matches(linkMenu)) {
             d.querySelector(panel).classList.remove('is-active');
             d.querySelector(panelBtn).classList.remove('is-active');
         }
-
     });
 }
 menuHamburguer('.panel-btn', '.panel', '.nav a');
